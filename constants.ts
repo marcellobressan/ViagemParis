61efebe819bc25d4031b1c31c8897808bd7b2af4
marcellobressan <37634213+marcellobressan@users.noreply.github.com>
2025-07-06 21:57:07 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,6 +1,2 @@
-
-// This file contains constants that do not require React/JSX.
-// It has been populated to resolve module import issues.
-
-export const SUPABASE_URL_PLACEHOLDER = 'https://ldwxmwezcrgaqqncdxdy.supabase.co';
-export const SUPABASE_ANON_KEY_PLACEHOLDER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxkd3htd2V6Y3JnYXFxbmNkeGR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMDIzMDYsImV4cCI6MjA2MzU3ODMwNn0.xDxvmlWt5S61NJQb7KTpn_xIGCH8Sx9ssI6qpl9eMOU';
+// This file is intentionally left empty to avoid module resolution issues.
+// Please import all constants from './constants' which will resolve to 'constants.tsx'.
